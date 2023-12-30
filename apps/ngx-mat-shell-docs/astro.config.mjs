57836dee605129cb356035a6decdeab85b9a4c10
Daniel Bersenkowitsch <accounts@projectbay.io>
2023-12-30 15:14:38 +0100

    added netlify config

--- apps/ngx-mat-shell-docs/astro.config.mjs
+++ apps/ngx-mat-shell-docs/astro.config.mjs
@@ -1,29 +1,32 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://projectbay.github.io',
-	base: '/os-projectbay',
-	integrations: [
-		starlight({
-			title: 'ngx-mat-shell',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: 'https://ngx-mat-shell.projectbay.io',
+  base: '/',
+  integrations: [starlight({
+    title: 'ngx-mat-shell',
+    social: {
+      github: 'https://github.com/withastro/starlight'
+    },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })],
+  output: "server",
+  adapter: netlify()
 });
